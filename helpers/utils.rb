87a483c4aa701699db58a18f5ca1e6f05cad67db
Dan Norris <daniel.norris@acquia.com>
2015-01-24 19:02:10 -0500

    Start moving around code and adding helpers

---
+++ helpers/utils.rb
@@ -0,0 +1,9 @@
+require 'sinatra/base'
+require_relative 'utils/name_handler'
+
+module Sinatra
+  module Utils
+  end
+
+  helpers Utils
+end
--- routes/secretary.rb
+++ routes/secretary.rb
@@ -1,4 +1,4 @@
-class Whimsy < Sinatra::Base
+class WhimsyApp < Sinatra::Base
 
   get '/secretary' do
     erb 'secretary/index'.to_sym, :locals => { :menu => 'menu' }
@@ -6,6 +6,10 @@ class Whimsy < Sinatra::Base
 
   get '/secretary/howto' do
     erb 'secretary/howto'.to_sym
+  end
+
+  get '/secretary/upload' do
+    erb 'secretary/upload'.to_sym
   end
 
   post '/secretary/upload' do
