374f126332a9710b9983c67a57614bbdba369780
Dan Norris <daniel.norris@acquia.com>
2015-02-07 17:30:41 -0500

    Add JSX build directory

--- public/js/secretary.js
+++ public/js/secretary.js
@@ -1,13 +1,13 @@
-var WorklistView = React.createClass({
+var WorklistView = React.createClass({displayName: "WorklistView",
   getInitialState: function() {
     return {data: []}
   },
   render: function() {
     return (
-      <div className="worklist">
-        <h2>Worklist</h2>
-        <WorkList data={this.state.data} />
-      </div>
+      React.createElement("div", {className: "worklist"}, 
+        React.createElement("h2", null, "Worklist"), 
+        React.createElement(WorkList, {data: this.state.data})
+      )
     );
   }
 });
