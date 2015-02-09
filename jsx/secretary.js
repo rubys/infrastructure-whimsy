c8f11830dd844f7d5f4d0505612f7449983aaace
Dan Norris <daniel.norris@acquia.com>
2015-02-08 21:41:01 -0500

    Start adding more to React components

--- jsx/secretary.js
+++ jsx/secretary.js
@@ -1,13 +1,42 @@
-var WorklistView = React.createClass({
-  getInitialState: function() {
-    return {data: []}
-  },
+var DocumentList = React.createClass({
   render: function() {
     return (
-      <div className="worklist">
-        <h2>Worklist</h2>
-        <WorkList data={this.state.data} />
+      <div className="documentList">
+        <Document filename="Test">Doc</Document>
       </div>
     );
   }
 });
+
+var Document = React.createClass({
+  render: function() {
+    return (
+      <div className="document">
+        <h4 className="filename">
+          {this.props.filename}
+        </h4>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+var WorkListView = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  render: function() {
+    return (
+      <div className="worklistBox">
+        <h3>Worklist</h3>
+        <DocumentList data={this.state.data} />
+      </div>
+    );
+  }
+});
+
+
+React.render(
+    <WorkListView />,
+    document.getElementById('worklist')
+);
--- public/js/secretary.js
+++ public/js/secretary.js
@@ -1,13 +1,42 @@
-var WorklistView = React.createClass({displayName: "WorklistView",
-  getInitialState: function() {
-    return {data: []}
-  },
+var DocumentList = React.createClass({displayName: "DocumentList",
   render: function() {
     return (
-      React.createElement("div", {className: "worklist"}, 
-        React.createElement("h2", null, "Worklist"), 
-        React.createElement(WorkList, {data: this.state.data})
+      React.createElement("div", {className: "documentList"}, 
+        React.createElement(Document, {filename: "Test"}, "Doc")
       )
     );
   }
 });
+
+var Document = React.createClass({displayName: "Document",
+  render: function() {
+    return (
+      React.createElement("div", {className: "document"}, 
+        React.createElement("h4", {className: "filename"}, 
+          this.props.filename
+        ), 
+        this.props.children
+      )
+    );
+  }
+});
+
+var WorkListView = React.createClass({displayName: "WorkListView",
+  getInitialState: function() {
+    return {data: []};
+  },
+  render: function() {
+    return (
+      React.createElement("div", {className: "worklistBox"}, 
+        React.createElement("h3", null, "Worklist"), 
+        React.createElement(DocumentList, {data: this.state.data})
+      )
+    );
+  }
+});
+
+
+React.render(
+    React.createElement(WorkListView, null),
+    document.getElementById('worklist')
+);
